cc933450992705f602ce0620605467841f42aa8a
Ziyaad Khan <happened234@gmail.com>
2023-05-11 15:17:15 +1200

    Internal.py

--- Internal.py
+++ Internal.py
@@ -1,1 +1,14 @@
-print("Hello world") 
+import tkinter as tk
+from tkinter import ttk
+
+#Creating window 
+window = tk.Tk()
+window.geometry("1024x600")
+window.title("Julies Party Hiring Store")
+
+#Creating a frame
+frame = tk.Frame(window)
+frame.pack()
+
+
+window.mainloop()
